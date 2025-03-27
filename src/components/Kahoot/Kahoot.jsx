6db4985b8756c2b8d5da8699cd6350c9778b7940
Felopater798 <felonabil77@gmail.com>
2025-03-27 19:08:05 +0200

    small change

--- src/components/Kahoot/Kahoot.jsx
+++ src/components/Kahoot/Kahoot.jsx
@@ -14,8 +14,8 @@ export default function Kahoot() {
     <p className='paragraph  dark:tw-text-white text-center'>Join the Kahoot game and enjoy the competition and <br/> challenge with your friends! 😊</p>
 
     <div className={`${styles.shad} , my-4 col-sm-12 p-5 rounded-4 bg-main dark:tw-bg-gray-900`}>
-      <div className="pin tw-bg-white dark:tw-bg-gray-800 mx-auto p-5 rounded-4" style={{width: '50%'}}>
-        <input type="text" placeholder='Game Pin'className={` ${styles.input} , p-4 form-control dark:tw-bg-gray-900`} />
+    <div className="pin tw-bg-white dark:tw-bg-gray-800  py-5 mx-auto tw-p-5 rounded-3 tw-w-full md:tw-w-[600px]">
+    <input type="text" placeholder='Game Pin'className={` ${styles.input} , p-4 w-100 dark:tw-bg-gray-900`} />
         <button className='bg-main w-100 text-white fs-2 fw-bolder rounded-3 py-3 mt-5 form-control'>Submit</button>
       </div>
         </div>
